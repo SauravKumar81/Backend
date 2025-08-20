576738a61b75acb2f9825f56876766f2e8cf8d78
Saurav Kumar <Sauravyt@gmail.com>
2025-08-21 00:33:11 +0530

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -34,12 +34,22 @@ const registerUser = asyncHandler(async (req , res) => {
         throw new ApiError(400, "User already exists");
     }
 
-    const avatarLocalPath = req.files("avatar")?.[0]?.path;
-    const coverImageLocalPath = req.files("coverImage")?.[0]?.path;
+     const avatarLocalPath = req.files?.avatar[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
+    
 
     if (!avatarLocalPath) {
-        throw new ApiError(400, "Both avatar and cover image are required");
+        throw new ApiError(400, "Both avatar ");
     }
+    // if (!coverImageLocalPath) {
+    //     throw new ApiError(400, "cover image are required");
+    // }
     const avatar = await uploadOnCloudinary(avatarLocalPath);
     const coverImage =  await uploadOnCloudinary(coverImageLocalPath);
 
